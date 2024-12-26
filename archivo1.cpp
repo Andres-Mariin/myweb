6e848585c5b22b77f4a05efe8e6ef43cce5a70a7
Andres marin <miniyluna02@gmail.com>
2024-12-26 13:29:24 -0500

    primeras lineas de codigo

---
+++ archivo1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int numero;
+
+    cout<<"ingrese un numero"<<endl;
+    cin>>numero;
+    if(numero<18){
+        cout<<"menor de edad"<<endl:
+    }else{
+        cout<<"mayor de edad"<<endl;
+    }
+
+    return 0;
+}
