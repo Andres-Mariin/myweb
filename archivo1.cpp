69d3cd20b45c9f6d68173745a86541c52a9c00a5
Andres marin <miniyluna02@gmail.com>
2024-12-26 14:55:01 -0500

    agregando caso switch

--- archivo1.cpp
+++ archivo1.cpp
@@ -2,14 +2,26 @@
 using namespace std;
 int main(){
 
-    int numero;
+    int numero, opcion;
 
     cout<<"ingrese un numero"<<endl;
     cin>>numero;
     if(numero<18){
         cout<<"menor de edad"<<endl;
     }else{
-        cout<<"mayor de edad"<<endl;
+        cout<<"opcion 1"<<endl;
+        cout<<"opcion 2"<<endl;
+        cout<<"elija una opcion"<<endl;
+        cin>>opcion;
+        switch(opcion){
+        case 1:
+        cout<<"hola mundo"<<endl;
+        break;
+        case 2:
+        cout<<"chao mundo";
+        break;
+        }
+
     }
 
     return 0;
