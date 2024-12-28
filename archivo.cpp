f3cc5ca274ae8af6d3bf6f875a0b8b5f18c406bb
Andres marin <miniyluna02@gmail.com>
2024-12-28 15:14:51 -0500

    agregando ciclos para verificacion de datos

--- archivo.cpp
+++ archivo.cpp
@@ -1,30 +1,46 @@
 #include<iostream>
 using namespace std;
-int main(){
+int main(){//inicio main
 
     int edad, opcion;
 
+    do{
+
     cout<<"Ingrese su edad"<<endl;
     cin>>edad;
-    if(edad<18 && edad >0){
-        cout<<"menor de edad"<<endl;
-    }else if(edad <=0 || edad >= 100){
-        cout<<"Ingrese solo valores validos"<<endl;
-    }else{
-        cout<<"opcion 1"<<endl;
-        cout<<"opcion 2"<<endl;
-        cout<<"elija una opcion"<<endl;
-        cin>>opcion;
-        switch(opcion){
-        case 1:
-        cout<<"hola mundo"<<endl;
-        break;
-        case 2:
-        cout<<"chao mundo";
-        break;
-        }
-
+    if(edad <=0 || edad >= 100){
+        cout<<"Igrese su edad"<<endl;
     }
+     } while(edad <=0 || edad >= 100);
 
+    if(edad < 18){
+        cout<<"Usted es menor de edad"<<endl;
+    }else{
+        cout<<"Bienvenido"<<endl<<endl;
+        cout<<"Menu"<<endl;
+        cout<<"Opcion 1"<<endl;
+        cout<<"Opcion 2"<<endl;
+
+        do{
+        cout<<"Elija una opcion del menu"<<endl;
+        cin>>opcion;
+        if(opcion <1 || opcion >2){
+            cout<<"Elija solo opciones validas"<<endl;
+        }
+        }while(opcion <1 || opcion >2);
+
+            switch(opcion){
+                case 1:
+                cout<<"Hola mundo"<<endl;
+                break;
+                case 2:
+                cout<<"Chao mundo"<<endl;
+                break;
+            
+            }
+        }
+    
+    
+    
     return 0;
-}
+}// fin main
