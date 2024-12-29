1aa80fbbf8e32453ea4d284eb9edb289e7194c01
Andres marin <miniyluna02@gmail.com>
2024-12-29 14:09:04 -0500

    creando menu -18

--- proyecto_git.cpp
+++ proyecto_git.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main(){//inicio main
 
     int edad, opcion, dia, mes, year;
+    int numero1, numero2, operacion;
+    char seccion;
     
     do{
         string meses[]={"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiempre", "octubre", "noviembre", "diciembre"};
@@ -21,7 +23,40 @@ int main(){//inicio main
      } while(edad <=0 || edad >= 100);
 
     if(edad < 18){
-        cout<<"Usted es menor de edad"<<endl;
+        cout<<"Usted es menor de edad"<<endl<<endl;
+        cout<<"le gustaria naver la seccion -18? Y - N"<<endl;
+        cin>>seccion;
+        if(seccion == 'y'){
+            cout<<"1 SUMA"<<endl;
+            cout<<"2 RESTA"<<endl;
+            cout<<"3 MUNLTIPLICACION"<<endl;
+            cout<<"4 DIVISION"<<endl;
+            cout<<"Elija una opcion"<<endl;
+            cin>>opcion;
+
+            switch(opcion){
+                 case 1:
+            cout<<" Vamos a sumar!!"<<endl;
+            cout<<"Ingresa dos numeros"<<endl;
+            cin>>numero1;
+            cin>>numero2;
+            operacion = numero1 + numero2;
+            cout<<"el resultado de la suma es "<<operacion<<endl;
+            break;
+
+            case 2:
+                cout<<" Vamos a restar!!"<<endl;
+                cout<<"Ingresa dos numeros"<<endl;
+                cin>>numero1;
+                cin>>numero2;
+                operacion = numero1 - numero2;
+                cout<<"el resultado de la resta es "<<operacion<<endl;
+            break;
+
+            }
+        
+        }
+
 
     }else{
 
